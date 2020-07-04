8d4057c93e3803714cae4f479cae86f71c8256ee
Shanmuga Priya S <shanmugapriyasukumaran30@gmail.com>
2020-07-04 10:55:16 +0530

    commit

---
+++ Programming/Twoponinters/Remove Duplicates From Sorted Array.cpp
@@ -0,0 +1,24 @@
+/*Remove duplicates from Sorted Array
+Given a sorted array, remove the duplicates in place such that each element appears only once and return the new length.
+
+Note that even though we want you to return the new length, make sure to change the original array as well in place
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+ Example:
+Given input array A = [1,1,2],
+Your function should return length = 2, and A is now [1,2]. */
+
+int Solution::removeDuplicates(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+for(auto s = A.begin(); s!=A.end(); ++s){
+    auto  e = s;
+    while((e+1)!=A.end() && *s==*(e+1)) ++e;
+    A.erase(s , e);
+}
+
+return A.size();}
+
